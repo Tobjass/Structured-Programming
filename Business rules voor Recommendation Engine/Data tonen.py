1a8aea785efbee5b5963e24b0f4ae1cf84ee4a72
Tobjass <74547189+Tobjass@users.noreply.github.com>
2021-03-17 17:19:39 +0100

    Update Data tonen.py

--- Business rules voor Recommendation Engine/Data tonen.py
+++ Business rules voor Recommendation Engine/Data tonen.py
@@ -9,24 +9,24 @@ con = psycopg2.connect(
 cur = con.cursor()
 
 
-def dataTonen(type, id):
-    if type == 1:
-        tekst = 'Producten'
-        cur.execute("select soortgelijk_id from soortgelijkproduct where product_id = '{}'".format(id))
-    elif type == 2:
-        tekst = 'Eerder bekeken producten'
-        cur.execute("select anderebekeken_id from anderebekeken where product_id = '{}'".format(id))
-    elif type == 3:
-        tekst = 'Eerder gekochte producten'
-        cur.execute("select anderekochten_id from anderekochten where product_id = '{}'".format(id))
-    ids = cur.fetchall()
-
+def tonen(id, tekst, ids):
     cur.execute("select name from products where product_id = '{}'".format(id))
     print("\n{} die vergelijkbaar zijn met '{}':\n".format(tekst, cur.fetchall()[0][0]))
 
-    for x in ids[0][0][1:len(ids[0][0])-1:].split(','):
+    for x in ids[0][0][1:len(ids[0][0]) - 1:].split(','):
         cur.execute("select name from products where product_id = '{}'".format(x))
         print("{}   ({})".format(cur.fetchall()[0][0], x))
 
 
-dataTonen(3, input("Product id?\n>> "))
+def dataTonen(id):
+    cur.execute("select soortgelijk_id from soortgelijkproduct where product_id = '{}'".format(id))
+    tonen(id, 'Producten', cur.fetchall())
+
+    cur.execute("select anderebekeken_id from anderebekeken where product_id = '{}'".format(id))
+    tonen(id, 'Eerder bekeken producten', cur.fetchall())
+
+    cur.execute("select anderekochten_id from anderekochten where product_id = '{}'".format(id))
+    tonen(id, 'Eerder gekochte producten', cur.fetchall())
+
+
+dataTonen(input("Product id?\n>> "))
