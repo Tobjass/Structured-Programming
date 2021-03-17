ccb36d66014b27654c8116ceb1536ac516942142
Tobjass <74547189+Tobjass@users.noreply.github.com>
2021-03-17 16:18:29 +0100

    Update Business rules voor Recommendation Engine.py
    Dubbele code weggehaald

--- Business rules voor Recommendation Engine/Business rules voor Recommendation Engine.py
+++ Business rules voor Recommendation Engine/Business rules voor Recommendation Engine.py
@@ -27,6 +27,28 @@ def soortgelijkProduct():
                     (producten[0], recommendations))
 
 
+def recommendationsAanmaken(productenlijst, type):
+    aantal = 0
+    for producten in products:
+        recommendations = []
+        for x in productenlijst:
+            cur.execute("select * from products where product_id = '{}'".format(x))
+            productenlijst = cur.fetchall()
+            if (producten[2] == productenlijst[0][2] and producten[5] == productenlijst[0][5]) and (
+                    producten[11] == productenlijst[0][11] and producten[12] == productenlijst[0][12]):
+                recommendations.append(x)
+
+        aantal += 1
+        print("{} / {}   ({:.1f}%)".format(aantal, 13676, aantal * 100 / 13676))
+
+        if type == 1:
+            cur.execute("insert into anderebekeken (product_id, anderebekeken_id) values (%s, %s)",
+                        (producten[0], recommendations))
+        elif type == 2:
+            cur.execute("insert into anderekochten (product_id, anderekochten_id) values (%s, %s)",
+                        (producten[0], recommendations))
+
+
 def andereBekeken():
     cur.execute("select viewed_before from visitors")
     viewed_before = cur.fetchall()
@@ -40,21 +62,7 @@ def andereBekeken():
         else:
             bekekenProducten.append(x[0][1:len(x[0])-1:])
 
-    aantal = 0
-    for producten in products:
-        recommendations = []
-        for x in bekekenProducten:
-            cur.execute("select * from products where product_id = '{}'".format(x))
-            bekekenProduct = cur.fetchall()
-            if (producten[2] == bekekenProduct[0][2] and producten[5] == bekekenProduct[0][5]) and (
-                    producten[11] == bekekenProduct[0][11] and producten[12] == bekekenProduct[0][12]):
-                recommendations.append(x)
-
-        aantal += 1
-        print("{} / {}   ({:.1f}%)".format(aantal, 13676, aantal * 100 / 13676))
-
-        cur.execute("insert into anderebekeken (product_id, anderebekeken_id) values (%s, %s)",
-                    (producten[0], recommendations))
+    recommendationsAanmaken(bekekenProducten, 1)
 
 
 def andereKochten():
@@ -69,21 +77,7 @@ def andereKochten():
         else:
             gekochteProducten.append(x[0][9:len(x[0]) - 3:])
 
-    aantal = 0
-    for producten in products:
-        recommendations = []
-        for x in gekochteProducten:
-            cur.execute("select * from products where product_id = '{}'".format(x))
-            gekochtProduct = cur.fetchall()
-            if (producten[2] == gekochtProduct[0][2] and producten[5] == gekochtProduct[0][5]) and (
-                    producten[11] == gekochtProduct[0][11] and producten[12] == gekochtProduct[0][12]):
-                recommendations.append(x)
-
-        aantal += 1
-        print("{} / {}   ({:.1f}%)".format(aantal, 13676, aantal * 100 / 13676))
-
-        cur.execute("insert into anderekochten (product_id, anderekochten_id) values (%s, %s)",
-                    (producten[0], recommendations))
+    recommendationsAanmaken(gekochteProducten, 2)
 
 
 con.commit()
