806222182dd47ce37095352eb077929210b7d7da
Tobjass <74547189+Tobjass@users.noreply.github.com>
2021-03-17 16:17:01 +0100

    Update Business rules voor Recommendation Engine.py
    Andere kochten functie toegevoegd

--- Business rules voor Recommendation Engine/Business rules voor Recommendation Engine.py
+++ Business rules voor Recommendation Engine/Business rules voor Recommendation Engine.py
@@ -52,9 +52,38 @@ def andereBekeken():
 
         aantal += 1
         print("{} / {}   ({:.1f}%)".format(aantal, 13676, aantal * 100 / 13676))
-        
+
         cur.execute("insert into anderebekeken (product_id, anderebekeken_id) values (%s, %s)",
-            (producten[0], recommendations))
+                    (producten[0], recommendations))
+
+
+def andereKochten():
+    cur.execute("select order_products from sessions where has_sale is true and order_products is not null")
+    order_products = cur.fetchall()
+
+    gekochteProducten = []
+    for x in order_products:
+        if ',' in x[0][9:len(x[0]) - 3:]:
+            for y in x[0][0:len(x[0])-1:].split(','):
+                gekochteProducten.append(y[9:len(y)-2:])
+        else:
+            gekochteProducten.append(x[0][9:len(x[0]) - 3:])
+
+    aantal = 0
+    for producten in products:
+        recommendations = []
+        for x in gekochteProducten:
+            cur.execute("select * from products where product_id = '{}'".format(x))
+            gekochtProduct = cur.fetchall()
+            if (producten[2] == gekochtProduct[0][2] and producten[5] == gekochtProduct[0][5]) and (
+                    producten[11] == gekochtProduct[0][11] and producten[12] == gekochtProduct[0][12]):
+                recommendations.append(x)
+
+        aantal += 1
+        print("{} / {}   ({:.1f}%)".format(aantal, 13676, aantal * 100 / 13676))
+
+        cur.execute("insert into anderekochten (product_id, anderekochten_id) values (%s, %s)",
+                    (producten[0], recommendations))
 
 
 con.commit()
